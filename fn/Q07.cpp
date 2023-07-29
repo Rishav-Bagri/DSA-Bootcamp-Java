91c4115ec90444222ae3e2daeb07c0dce23f39d0
Rishav <bagririshav01@gmail.com>
2023-07-29 16:03:07 +0530

    Define a method to find out if a number is prime or not

---
+++ fn/Q07.cpp
@@ -0,0 +1,35 @@
+// Define a method to find out if a number is prime or not.
+
+#include <iostream>
+using namespace std;
+
+int prime(int n){
+    int count=0;
+    for (int i = 1; i < n; i++)
+    {
+        int a=n%i;
+        if (a==0)
+        {
+            count++;
+            if (count==2)
+            {
+                return false;
+                break;
+            }                                    
+        }
+                
+    } 
+    return true;   
+}
+
+int main(){
+    int a,n;
+    cin>>n;
+
+    
+
+    a=prime(n);
+    cout<<a;
+
+    return 0;
+}
